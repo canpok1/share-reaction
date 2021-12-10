784df9169a01541d12e45d0ce8700df0f60d92c5
canpok1 <6isstrong@gmail.com>
2021-12-10 16:49:16 +0000

    拍手機能を追加

--- server.js
+++ server.js
@@ -6,15 +6,21 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 8080;
 const INDEX = '/index.html';
 
-const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+app.get('/', (req, res) => res.sendFile(__dirname + INDEX));
+app.use(express.static(`${__dirname}/public`));
+
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
+    socket.on('clap', (name) => {
+        console.log('clapped, name:' + name);
+        socket.broadcast.emit('clapped', name);
+    });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
