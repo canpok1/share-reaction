647af6bbd26c6862d5b3dff6a057981f0f28242c
canpok1 <6isstrong@gmail.com>
2021-12-11 05:58:19 +0000

    音量をスライダー化し画面の見た目を調整

--- public/js/room.js
+++ public/js/room.js
@@ -1,18 +1,26 @@
 let params = {
     socket: null,
-    enablePlay: false,
-    roomId: ""
+    mute: false,
+    volume: 0,
+    roomId: ''
 };
+
+const audios = {
+    clap: '/audio/clap.m4a'
+};
+
+const VOLUME_MAX = 100;
+const VOLUME_MIN = 0;
 
 function init(roomId) {
     params.socket = io();
     params.roomId = roomId;
-    params.socket.on("connect", () => {
-        console.log("connected");
+    params.socket.on('connect', () => {
+        console.log('connected');
         join();
     });
-    params.socket.on("disconnect", () => {
-        console.log("disconnected");
+    params.socket.on('disconnect', () => {
+        console.log('disconnected');
         leave();
     });
     params.socket.on('reaction', (reactionType) => {
@@ -21,44 +29,58 @@ function init(roomId) {
 }
 
 function join() {
-    params.socket.emit("join", params.roomId);
-    document.getElementById('clap-button').removeAttribute("disabled");
+    params.socket.emit('join', params.roomId);
+    document.getElementById('clap-button').disabled = false;
 }
 
 function leave() {
-    document.getElementById('clap-button').setAttribute("disabled", true);
+    document.getElementById('clap-button').disabled = true;
 }
 
-function setEnable(enable) {
-    params.enablePlay = enable;
+function setMute(mute) {
+    params.mute = mute;
+}
+
+function setVolume(volume) {
+    if (volume > VOLUME_MAX) {
+        params.volume = VOLUME_MAX;
+    } else if (volume < VOLUME_MIN) {
+        params.volume = VOLUME_MIN;
+    } else {
+        params.volume = volume;
+    }
+    console.log(`volume: ${params.volume}`);
 }
 
 function play(reactionType) {
-    if (!params.enablePlay) {
+    if (params.mute) {
         return;
     }
 
-    const audioPath = "/audio/clap.m4a";
     try {
         const audioCtx = new AudioContext();
 
         const audioEle = new Audio();
-        audioEle.src = audioPath;
+        audioEle.src = audios[reactionType];
         audioEle.autoplay = true;
-        audioEle.preload = "auto";
-        audioEle.addEventListener("ended", function () {
+        audioEle.preload = 'auto';
+        audioEle.addEventListener('ended', function () {
             audioCtx.close();
         });
-
         const audioSourceNode = audioCtx.createMediaElementSource(audioEle);
 
-        audioSourceNode.connect(audioCtx.destination);
+        const gainNode = audioCtx.createGain();
+        gainNode.gain.value = params.volume / VOLUME_MAX;
+
+        audioSourceNode.connect(gainNode);
+        gainNode.connect(audioCtx.destination)
+
     } catch (error) {
-        console.log("error occured!!! error:" + error);
+        console.log('error occured!!! error:' + error);
     }
 }
 
 function reaction(reactionType) {
-    params.socket.emit("reaction", reactionType);
+    params.socket.emit('reaction', reactionType);
     play(reactionType);
 }
